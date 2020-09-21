288d7e6a2797a4bbae6a1caa98991dcc608eb9e4
Aniket <aniketshetyeajs@gmail.com>
2020-09-21 17:54:50 +0530

    first commit

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const path = require('path')
+const cors = require('cors');
 
 const app = express();
 
 connectDB();
 
 app.use(express.json({ extended: false }));
+app.use(cors())
 
-
-
+app.get('/', (req, res) => res.send('server running'));
 
 // routes
 
@@ -17,15 +17,6 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
-
-//serve static assets in production
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'))
-
-    app.get('*' (req,res) => {
-        res.sendFile(path.resolve(__dirname, 'client','build', 'index.html'))
-    })
-}
 
 const PORT = process.env.PORT || 8000;
 
